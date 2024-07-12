e96844b712db757d9e8537ee2891ffbea01a4b39
Mohammad Ahmad <eng.2002.mohammad@gmail.com>
2024-07-12 10:54:40 +0300

    Complete Step 1

--- StringCalculator/StringCalculator/Program.fs
+++ StringCalculator/StringCalculator/Program.fs
@@ -2,4 +2,21 @@
 
 open System
 
-printfn "Hello F#"
+    
+
+let Add (numbers:string):int =
+    match numbers with
+    | "" -> 0
+    | _ -> match numbers.Split(",")  with
+           | [| num1 |] -> (int num1)
+           | [| num1 ; num2 |] -> (int num1) + (int num2)
+           | _ -> failwith "Not Supported"
+
+//Step 1:
+printfn "%d" (Add "")
+printfn "%d" (Add "1")
+printfn "%d" (Add "1,2")
+printfn "%d" (Add "-1,4")
+printfn "%d" (Add "1,2,3,4") // thorw an exception
+
+//-------
